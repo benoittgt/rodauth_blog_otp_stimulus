0351eb9316c873e73e05e44dcdce82961dbf1d17
Janko Marohnić <janko.marohnic@gmail.com>
2020-11-18 17:07:33 +0100

    Add Bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,6 @@ Turbolinks.start()
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap/dist/css/bootstrap'
